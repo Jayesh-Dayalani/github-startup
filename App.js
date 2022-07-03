4297e23d6ba61c74ac6941b7c368c6788a5be9bf
Jayesh Dayalani <jayeshdayalanii@gmail.com>
2022-07-03 19:13:05 +0530

    sample file added

---
+++ App.js
@@ -0,0 +1,24 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ComponentScreen from "./src/screens/ComponentScreen";
+import ImageScreen from "./src/screens/ImagesScreen";
+
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    List: ListScreen,
+    Component: ComponentScreen,
+    Image: ImageScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
